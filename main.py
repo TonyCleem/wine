3b301eefca3da722c324180b4e1ae4ac832863d1
Tony Cleem <tonycleem@gmail.com>
2025-04-20 17:16:40 +0700

    update site

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 def get_needed_form(duration):
     declensions = ['год', 'года']
     needed_form = 'лет'
-
     remainder = duration % 10
     tens_remainder = duration % 100
 
@@ -33,16 +32,13 @@ def main():
         loader=FileSystemLoader('.'), 
         autoescape=select_autoescape(['html', 'xml'])
         )
-
     template = env.get_template('template.html')
 
     now = datetime.datetime.now()
-    first_data = 1920
-    duration = now.year - first_data
+    duration = now.year - 1920
     needed_form = get_needed_form(duration)
 
     excel_file = 'wine3.xlsx'
-
     wine_from_excel = pandas.read_excel(
         excel_file, 
         sheet_name='Лист1', 
@@ -50,9 +46,7 @@ def main():
         usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция']
         )
     wines = wine_from_excel.to_dict(orient='records')
-
     wines = update_range_wines(wines)
-
     rendered_page = template.render(
         years=duration,
         years_string=needed_form,
