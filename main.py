5dba5bad325c181d0c032d5be8be1ff695865960
Tony Cleem <tonycleem@gmail.com>
2025-04-26 17:32:51 +0700

    fix bags

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import datetime
-import pandas
 import collections
 import os
-from dotenv import load_dotenv
+import pandas
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
@@ -11,11 +11,8 @@ FOUNDATION_YEAR = 1920
 
 
 def get_correct_tense_declination(total_years_together):
-    declensions = [
-        'год',
-        'года'
-    ]
-    correct_tense_declination = 'лет'
+    declensions = ["год", "года"]
+    correct_tense_declination = "лет"
 
     remainder = total_years_together % 10
     tens_remainder = total_years_together % 100
@@ -28,10 +25,10 @@ def get_correct_tense_declination(total_years_together):
     return correct_tense_declination
 
 
-def grouping_wines_by_categories(wines):
+def group_wines_by_category(wines):
     wines_by_category = collections.defaultdict(list)
     for wine in wines:
-        wines_by_category[wine['Категория']].append(wine)
+        wines_by_category[wine["Категория"]].append(wine)
     return wines_by_category
 
 
@@ -39,46 +36,49 @@ def main():
     load_dotenv()
 
     env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-        )
-    template = env.get_template('template.html')
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(
+            [
+                "html",
+                "xml",
+            ]
+        ),
+    )
+    template = env.get_template("template.html")
 
     now = datetime.datetime.now()
     total_years_together = now.year - FOUNDATION_YEAR
-    correct_tense_declination = get_correct_tense_declination(
-        total_years_together
-    )
+    correct_tense_declination = get_correct_tense_declination(total_years_together)
 
     excel_filename = os.getenv("EXCEL_FILE")
     wine_from_excel = pandas.read_excel(
         excel_filename,
-        sheet_name='Лист1',
+        sheet_name="Лист1",
         keep_default_na=False,
         usecols=[
-            'Категория',
-            'Название',
-            'Сорт',
-            'Цена',
-            'Картинка',
-            'Акция'
-        ]
-        )
-    wines = wine_from_excel.to_dict(orient='records')
-    wines_by_category = grouping_wines_by_categories(wines)
+            "Категория",
+            "Название",
+            "Сорт",
+            "Цена",
+            "Картинка",
+            "Акция",
+        ],
+    )
+    wines = wine_from_excel.to_dict(orient="records")
+    wines_by_category = group_wines_by_category(wines)
 
     rendered_page = template.render(
         years=total_years_together,
         years_string=correct_tense_declination,
-        wines=wines_by_category
-        )
+        wines=wines_by_category,
+    )
 
-    with open('index.html', 'w', encoding="utf8") as excel_filename:
+    with open("index.html", "w", encoding="utf8") as excel_filename:
         excel_filename.write(rendered_page)
 
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
